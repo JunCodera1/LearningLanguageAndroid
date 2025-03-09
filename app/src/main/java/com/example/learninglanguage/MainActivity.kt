81cb65a6d52e1576208b5e2649ebeed671b94f8a
JunCodera1 <minhtien0601200@gmail.com>
2025-03-09 22:31:26 +0700

    In fix top nav

--- app/src/main/java/com/example/learninglanguage/MainActivity.kt
+++ app/src/main/java/com/example/learninglanguage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learninglanguage
 
+import TopAppBarSection
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,16 +9,17 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.learninglanguage.navigation.Navigation
 import com.example.learninglanguage.ui.components.BottomNavigation
 import com.example.learninglanguage.ui.theme.LearningLanguageTheme
 import com.example.learninglanguage.viewmodel.AuthState
 import com.example.learninglanguage.viewmodel.AuthViewModel
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,39 +28,32 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LearningLanguageTheme {
-                val navController = rememberNavController()
-                val authState by authViewModel.authState.observeAsState(AuthState.Unauthenticated) // Lắng nghe trạng thái đăng nhập
-
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    bottomBar = {
-                        if (authState is AuthState.Authenticated) {
-                            BottomNavigation(
-                                navController = navController,
-                                authViewModel = authViewModel
-                            )
-                        }
-                    }
-                ) { paddingValues ->
-                    Navigation(
-                        modifier = Modifier.padding(paddingValues),
-                        authViewModel = authViewModel,
-                        navController = navController
-                    )
-                }
+                MainScreen(authViewModel)
             }
         }
     }
 }
 
+@Composable
+fun MainScreen(authViewModel: AuthViewModel) {
+    val navController = rememberNavController()
+    val authState by authViewModel.authState.observeAsState(AuthState.Unauthenticated)
 
-//@Preview
-//@Composable
-//fun MyBottomBarPreview(){
-//    val authViewModel = AuthViewModel() // ⚠️ Đây chỉ để preview, không nên dùng trực tiếp
-//
-//    LearningLanguageTheme {
-//        val navController = rememberNavController() // ⚡ Tạo NavController cho Preview
-//        BottomNavigation(modifier = Modifier, navController = navController, authViewModel = authViewModel)
-//    }
-//}
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBarSection(navController, authViewModel) },
+        bottomBar = {
+            if (authState is AuthState.Authenticated) {
+                BottomNavigation(navController, authViewModel)
+            }
+        }
+    ) { paddingValues ->
+        Navigation(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            authViewModel = authViewModel,
+            navController = navController
+        )
+    }
+}
